5d14cf1d48a4fc287b2f3c02d1394029dcc620c9
RastkoAmun <raciosh@gmail.com>
2022-08-31 08:29:50 -0700

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Person from './components/Person'
 
-function App() {
+const App = () => {
+  const [persons, setPerson] = useState([{
+    name: 'Magnus Carlsen',
+    number: '236-665-9989'
+  }])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+    }
+
+    const isFound = persons.find(person => 
+      person.name === newName)
+
+    if(isFound){
+      alert(`${newPerson.name} is already added to phonebook`)
+    }else{
+      setPerson(persons.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameInput = (event) => {
+    setNewName(event.target.value);
+  }
+  const handleNumberInput = (event) => {
+    setNewNumber(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameInput}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberInput}/>
+        </div>
+        <div>
+          <button type='submit' onClick={addPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => 
+          <Person name={person.name} phoneNumber={person.number} key={person.name}/>)}
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/Person.js
@@ -0,0 +1,9 @@
+const Person = ({name, phoneNumber}) => {
+  return(
+    <div>
+      {name}: {phoneNumber}
+    </div>
+  )
+}
+
+export default Person;
